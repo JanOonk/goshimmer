b70babeb38be22d5ba59d13da653b04ae523c731
Gal Rogozinski <galrogogit@gmail.com>
2021-06-28 01:21:27 +0300

    add cached unconfirmed tx dependencies

--- packages/tangle/storage.go
+++ packages/tangle/storage.go
@@ -1052,47 +1052,6 @@ func (m *MissingMessage) ObjectStorageValue() (result []byte) {
 	return
 }
 
-// region UnconfirmedTxDependency //////////////////////////////////////////////////////////////////////////////////////
-
-// UnconfirmedTxDependency maps a transaction to all of the transactions that create its inputs which are not yet confirmed
-type UnconfirmedTxDependency struct {
-	objectstorage.StorableObjectFlags
-
-	txID           ledgerstate.TransactionID
-	txDependencies ledgerstate.TransactionIDs
-}
-
-// NewUnconfirmedTxDependency creates an empty mapping for txID
-func NewUnconfirmedTxDependency(txID ledgerstate.TransactionID) *UnconfirmedTxDependency {
-	return &UnconfirmedTxDependency{
-		txID:           txID,
-		txDependencies: make(ledgerstate.TransactionIDs, 0),
-	}
-}
-
-// AddDependency adds a transaction id dependency
-func (u *UnconfirmedTxDependency) AddDependency(txID ledgerstate.TransactionID) {
-	u.txDependencies[txID] = types.Void
-}
-
-func (u *UnconfirmedTxDependency) Update(other objectstorage.StorableObject) {
-	panic("implement me")
-}
-
-func (u *UnconfirmedTxDependency) ObjectStorageKey() []byte {
-	return u.txID.Bytes()
-}
-
-func (u *UnconfirmedTxDependency) ObjectStorageValue() []byte {
-	marshalUtil := marshalutil.New(ledgerstate.TransactionIDLength * len(u.txDependencies))
-	for dependency := range u.txDependencies {
-		marshalUtil.WriteBytes(dependency.Bytes())
-	}
-	return marshalUtil.Bytes()
-}
-
-// endregion ///////////////////////////////////////////////////////////////////////////////////////////////////////////
-
 // region CachedMissingMessage /////////////////////////////////////////////////////////////////////////////////////////
 
 // CachedMissingMessage is a wrapper for the generic CachedObject returned by the object storage that overrides the
@@ -1142,6 +1101,101 @@ func (c *CachedMissingMessage) Consume(consumer func(missingMessage *MissingMess
 // String returns a human readable version of the CachedMissingMessage.
 func (c *CachedMissingMessage) String() string {
 	return stringify.Struct("CachedMissingMessage",
+		stringify.StructField("CachedObject", c.Unwrap()),
+	)
+}
+
+// endregion ///////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// region UnconfirmedTxDependency //////////////////////////////////////////////////////////////////////////////////////
+
+// UnconfirmedTxDependency maps a transaction to all of the transactions that create its inputs which are not yet confirmed
+type UnconfirmedTxDependency struct {
+	objectstorage.StorableObjectFlags
+
+	txID           ledgerstate.TransactionID
+	txDependencies ledgerstate.TransactionIDs
+}
+
+// NewUnconfirmedTxDependency creates an empty mapping for txID
+func NewUnconfirmedTxDependency(txID ledgerstate.TransactionID) *UnconfirmedTxDependency {
+	return &UnconfirmedTxDependency{
+		txID:           txID,
+		txDependencies: make(ledgerstate.TransactionIDs, 0),
+	}
+}
+
+// AddDependency adds a transaction id dependency
+func (u *UnconfirmedTxDependency) AddDependency(txID ledgerstate.TransactionID) {
+	u.txDependencies[txID] = types.Void
+}
+
+func (u *UnconfirmedTxDependency) Update(other objectstorage.StorableObject) {
+	panic("implement me")
+}
+
+func (u *UnconfirmedTxDependency) ObjectStorageKey() []byte {
+	return u.txID.Bytes()
+}
+
+func (u *UnconfirmedTxDependency) ObjectStorageValue() []byte {
+	marshalUtil := marshalutil.New(ledgerstate.TransactionIDLength * len(u.txDependencies))
+	for dependency := range u.txDependencies {
+		marshalUtil.WriteBytes(dependency.Bytes())
+	}
+	return marshalUtil.Bytes()
+}
+
+// endregion ///////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// region CachedUnconfirmedTxDependency ////////////////////////////////////////////////////////////////////////////////
+
+// CachedUnconfirmedTxDependency is a wrapper for the generic CachedObject returned by the object storage that overrides the
+// accessor methods with a type-casted one.
+type CachedUnconfirmedTxDependency struct {
+	objectstorage.CachedObject
+}
+
+// ID returns the UnconfirmedTxDependency of the requested MissingMessage.
+func (c *CachedUnconfirmedTxDependency) ID() (id ledgerstate.TransactionID) {
+	id, _, err := ledgerstate.TransactionIDFromBytes(c.Key())
+	if err != nil {
+		panic(err)
+	}
+	return
+}
+
+// Retain marks the CachedObject to still be in use by the program.
+func (c *CachedUnconfirmedTxDependency) Retain() *CachedUnconfirmedTxDependency {
+	return &CachedUnconfirmedTxDependency{c.CachedObject.Retain()}
+}
+
+// Unwrap is the type-casted equivalent of Get. It returns nil if the object does not exist.
+func (c *CachedUnconfirmedTxDependency) Unwrap() *UnconfirmedTxDependency {
+	untypedObject := c.Get()
+	if untypedObject == nil {
+		return nil
+	}
+
+	typedObject := untypedObject.(*UnconfirmedTxDependency)
+	if typedObject == nil || typedObject.IsDeleted() {
+		return nil
+	}
+
+	return typedObject
+}
+
+// Consume unwraps the CachedObject and passes a type-casted version to the consumer (if the object is not empty - it
+// exists). It automatically releases the object when the consumer finishes.
+func (c *CachedUnconfirmedTxDependency) Consume(consumer func(unconfirmedTxDependency *UnconfirmedTxDependency), forceRelease ...bool) (consumed bool) {
+	return c.CachedObject.Consume(func(object objectstorage.StorableObject) {
+		consumer(object.(*UnconfirmedTxDependency))
+	}, forceRelease...)
+}
+
+// String returns a human readable version of the CachedMissingMessage.
+func (c *CachedUnconfirmedTxDependency) String() string {
+	return stringify.Struct("CachedUnconfirmedTxDependency",
 		stringify.StructField("CachedObject", c.Unwrap()),
 	)
 }
